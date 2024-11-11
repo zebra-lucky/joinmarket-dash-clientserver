58aaebd94f31a5d02132d5c05d9aad5678258bc6
zebra-lucky <zebra.lucky@gmail.com>
2024-11-11 22:25:57 +0200

    add simple build.sh

---
+++ contrib/build-linux/pyinstaller-build/build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+
+PROJECT_ROOT="$(dirname "$(readlink -e "$0")")/../../.."
+VENVPATH=$PROJECT_ROOT/venv
+JM_ROOT=$PROJECT_ROOT
+
+
+sudo apt-get install python3-dev python3-pip python3-venv git build-essential automake pkg-config libtool libffi-dev libssl-dev
+
+
+python3.8 -m venv $VENVPATH
+
+
+. $VENVPATH/bin/activate
+
+
+pip install .[gui]
+
+
+rm -rf deps
+mkdir -p deps
+cd deps
+git clone https://github.com/bitcoin-core/secp256k1.git
+cd secp256k1
+git checkout v0.6.0
+./autogen.sh
+./configure --prefix $JM_ROOT --enable-module-recovery --enable-experimental --enable-module-ecdh --enable-benchmark=no
+make
+make check
+make install
+cd ../..
+
+exit 0
+
+
+rm -rf libsodium
+git clone https://github.com/jedisct1/libsodium.git
+cd libsodium
+git checkout 1.0.18-RELEASE
+./autogen.sh
+./configure --prefix $JM_ROOT
+make check
+sudo make install
+cd ..
